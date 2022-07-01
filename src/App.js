d84b73ec869b74ef009a20d3f69983ba469ca846
leandroteixeira97 <leandro.teixeira.sjc@gmail.com>
2022-07-01 18:11:28 -0300

    feat: Lógica inicial de paginação, escolha das palavras de forma aleatória

--- src/App.js
+++ src/App.js
@@ -1,9 +1,63 @@
+// Styles
 import './App.css';
 
+// React
+import { useCallback, useEffect, useState } from 'react';
+
+// Data
+import { wordsList } from './data/words';
+
+// Components
+import StartScreen from './components/StartScreen';
+import Game from './components/Game';
+import GameOver from './components/GameOver';
+
+const stages = [
+  { id: 1, name: 'start' },
+  { id: 2, name: 'game' },
+  { id: 3, name: 'end' }
+];
+
 function App() {
+  const [gameStage, setGameStage] = useState(stages[0].name);
+  const [words] = useState(wordsList);
+
+  const [pickedWord, setPickedWord] = useState("");
+  const [pickedCategory, setPickedCategory] = useState("");
+  const [letters, setLetters] = useState([]);
+
+  const pickWordAndCategory = () => {
+    const categories = Object.keys(words);
+    const category = categories[Math.floor(Math.random() * (categories.length))];
+    const word = words[category][Math.floor(Math.random() * (words[category].length))];
+    return {word, category}
+  };
+
+  // Starts the game
+  const startGame = () => {
+    const { word, category } = pickWordAndCategory();
+    let wordLetters = word.toLowerCase().split("");
+    setPickedCategory(category);
+    setPickedWord(word);
+    setLetters(wordLetters);
+    setGameStage(stages[1].name);
+  };
+
+  // Process the inputs
+  const verifyLetter = () => {
+    setGameStage(stages[2].name);
+  };
+
+  // Restarts the game
+  const retry = () => {
+    setGameStage(stages[0].name);
+  };
+
   return (
     <div className="App">
-      <h2>Secret Word</h2>
+      {gameStage === 'start' && <StartScreen startGame={startGame} />}
+      {gameStage === 'game' && <Game verifyLetter={verifyLetter} />}
+      {gameStage === 'end' && <GameOver retry={retry} />}
     </div>
   );
 }
